08c3986e499542ee2fe2e49bb7688e31a1ee6226
Connor <connorhipp1130@gmail.com>
2025-02-05 05:25:14 -0500

    Update week_1_intro.py

--- 01_Python_Basics/week_1_intro.py
+++ 01_Python_Basics/week_1_intro.py
@@ -65,6 +65,9 @@ print(confirmation)
 # Convert the ticker symbol to lowercase.
 # Print "Stock selected: aapl" (in lowercase).
 
+stock_ticker = "AAPL"
+print(f"stock selected: {stock_ticker.lower()}\n")
+
 # Exercise 5: Lists & Dictionaries
 
 # Task:
@@ -72,6 +75,19 @@ print(confirmation)
 # Create a list of three stock symbols.
 # Create a dictionary that maps each symbol to the number of shares owned.
 # Print the holdings using a loop.
+
+stock_symbols = ["AAPL", "TSLA", "AMZN"]  # list of stock symbols
+# dictionary of stock symbols and number of shares outstanding
+shares_outstanding = {
+    "AAPL": 300000,
+    "TSLA": 10000,
+    "AMZN": 3000
+}
+# print the holdings using a loop
+print("Shares Outstanding:")
+for symbol in stock_symbols:
+    print(f"{symbol}: {shares_outstanding[symbol]}")
+print()
 
 # Exercise 6: Simple Conditional Logic
 
@@ -81,6 +97,18 @@ print(confirmation)
 # If the price is above $180, print "AAPL is expensive!".
 # Otherwise, print "AAPL is at a reasonable price.".
 
+while True:
+    try:
+        aapl_price = float(input("What is the price of Apple, Inc. stock?\n"))
+        if aapl_price <= 0:
+            print("Please enter a positive price.")
+        break
+    except ValueError:
+        print("Please enter a numerical input.")
+confirmation_text = f"You have entered the price of AAPL to be ${
+    aapl_price:.2f}."
+print(confirmation_text)
+
 # Exercise 7: Investment Growth Calculation
 
 # Task:
@@ -88,3 +116,14 @@ print(confirmation)
 # Assume an initial investment and a growth rate (e.g., 5%).
 # Calculate the investment value after 1 year.
 # Print the result.
+
+initial_investment = float(
+    input("How much money would you like to invest in AAPL?\n"))
+time_horizon = float(
+    input("How many years will you be holding stock in AAPL?\n"))
+confirmation_investment = f"Confirmed! Your ${
+    initial_investment:.2f} order for AAPL for {time_horizon} has been placed."
+print(confirmation_investment)
+f_value = initial_investment * (1 + .05) ** time_horizon
+print(f"In {time_horizon} years, your ${
+      initial_investment:.2f} will be worth ${f_value:.2f}")
